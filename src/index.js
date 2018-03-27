635c4a5905e152684e948095174e80e1d2ac79db
Eric Pham <ericpham380@gmail.com>
2018-03-27 08:30:14 -0700

    Installed and imported lodash dependency to index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
+import _ from 'lodash';
+
 function component() {
   // create a div and initialize it to a variable
   var element = document.createElement('div');
 
   // Lodash, currently included via a script, is required for this line to work
+  // Lodash is now imported using top script.
   element.innerHTML = _.join(['Hello', 'Webpack'], ', ');
 
   return element;
