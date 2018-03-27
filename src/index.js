cd4fe554bd64108263308adfec7e7324a9ca60b2
Eric Pham <ericpham380@gmail.com>
2018-03-27 09:41:32 -0700

    Added and loaded images

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import './style.css';
+import webpack_icon from './webpack-icon.svg';
 
 function component() {
   // create a div and initialize it to a variable
@@ -9,8 +10,13 @@ function component() {
   // Lodash is now imported using top script.
   element.innerHTML = _.join(['Hello', 'Webpack'], ', ');
 
-  // Add class to this element
+  // Add class to this existing div element
   element.classList.add('hello');
+
+  // Adding image to this existing div element
+  var icon = new Image();
+  icon.src = webpack_icon;
+  element.appendChild(icon);
 
   return element;
 }
