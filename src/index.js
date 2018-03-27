8811c71eb84564d2f991a34ef78139d0181c7507
Eric Pham <ericpham380@gmail.com>
2018-03-27 09:27:14 -0700

    Added style.css asset and imported to index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+import './style.css';
 
 function component() {
   // create a div and initialize it to a variable
@@ -7,6 +8,9 @@ function component() {
   // Lodash, currently included via a script, is required for this line to work
   // Lodash is now imported using top script.
   element.innerHTML = _.join(['Hello', 'Webpack'], ', ');
+
+  // Add class to this element
+  element.classList.add('hello');
 
   return element;
 }
